74db2936ca4eba49d7ff7aafef18551650ace9a8
John M. Kimball <jmkimball42@gmail.com>
2019-12-10 08:59:00 -0500

    Update dyna_api.sh

--- dyna_api.sh
+++ dyna_api.sh
@@ -4,11 +4,11 @@
 set -xv
 
 # Some vars
-DYN=https://tgo745.dynatrace-managed.com/e/7571065c-f052-471e-a3d7-f99d529548bb
-P_TOK=Jm7Plp67Qa-jouC1s9YPN
-NP_TOK=4fkDCDp_T7-kxpa_wrzZy
+DYN=
+P_TOK=
+NP_TOK=
 HOST=u060ctma11a
 
 #curl -L -H "Authorization: Api-Token $P_TOK" $DYN/api/v1/entity/infrastructure/hosts/$HOST
 
-curl -X GET $DYN/api/v1/entity/infrastructure/hosts -H 'Authorization: Api-Token Jm7Plp67Qa-jouC1s9YPN'
+curl -X GET $DYN/api/v1/entity/infrastructure/hosts -H 'Authorization: Api-Token 
